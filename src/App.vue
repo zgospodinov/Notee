a48990391813b823a52ede443447fff5dbdbb9b5
zdravki4 <zdravko.gospodinov@gmail.com>
2019-03-21 07:18:53 +0200

    Aside navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <AsideMenu/>
-    <button class="button is-primary">Button</button>
+    <div class="wrapper">
+      <h1>Notee</h1>
+      <div class="dashboard">
+        <AsideMenu/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import AsideMenu from './components/AsideMenu.vue'
+import AsideMenu from "./components/AsideMenu.vue";
 // import bulma from './node_modules/bulma/css/bulma.css.css"'
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     AsideMenu
   }
-}
+};
 </script>
 
 <style>
-
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+.wrapper{
+  background-color:cyan;
+  margin: 0 auto;
+  width:95%;
+  height:100%;  
+}
+
+.dashboard {
+  width: 25%;
+  float: left;
 }
 </style>
--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -1,32 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>Hi</h1>
+  <div class="navigation">
+    <aside class="menu">
+      <p class="menu-label">General</p>
+      <ul class="menu-list">
+        <li>
+          <a class="is-size-7" @click="showTasksView">Tasks</a>
+        </li>
+      </ul>
+      <p class="menu-label">Administration</p>
+      <ul class="menu-list">
+        <li>
+          <a class="is-size-7" @click="showTeamSettings">Team Settings</a>
+        </li>
+      </ul>
+    </aside>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'AsideMenu',
-  props: {
-    
+  name: "AsideMenu",
+  props: {},
+  methods: {
+    showTasksView() {
+      alert("task views");
+    },
+    showTeamSettings() {
+      alert("team view settings");
+    }
   }
-}
+};
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.navigation {
+  background-color: rgb(196, 196, 196);
+  padding: 5px;
+  width: 100%;
+}
+p {
+  text-align: left;
+}
 h3 {
   margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
   padding: 0;
+  width: 100%;
 }
 li {
   display: inline-block;
   margin: 0 10px;
+  width: 100%;
 }
 a {
+  text-align: left;
   color: #42b983;
+  padding-left: 5px;
+  margin: 0px;
+  width: 100%;
+  /* background-color:blanchedalmond; */
 }
 </style>
